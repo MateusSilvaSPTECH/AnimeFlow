1702148e11d178808f09b9adb22f4b54ef23e633
MateusSilvaSPTECH <mateus.ssouza@sptech.school>
2025-05-05 21:05:40 -0300

    Pagina do select all estruturada/Footer completo

---
+++ assets/js/menudropdown.js
@@ -0,0 +1,6 @@
+var sub_menu = document.getElementById("drop_menu");
+function mostrarDropDown() {
+    console.log('entrei na fuc');
+    sub_menu.classList.toggle("open_menu");
+    console.log('entrei na passei');
+}
